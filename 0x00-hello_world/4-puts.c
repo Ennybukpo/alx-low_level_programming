c275692e5dd27746ff38d946bd513bda54d51b0e
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-09 05:44:24 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-include <stdio.h>
+#include <stdio.h>
 /**
- *main -main block
- *return: 0
+ * main-entry point for program
+ *
+ * Return:0 if no errors, reurn non zeros i errors
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
