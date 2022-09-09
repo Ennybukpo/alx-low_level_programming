af501abaa03a087e11391b867b995a84495d35f8
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-09 16:29:26 -0700

    new commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,9 @@ int main(void)
 {
 	int num;
 
-	forv(num = 0; num <= 9; num++)
-	{putchar((num % 10) + '0');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
 
