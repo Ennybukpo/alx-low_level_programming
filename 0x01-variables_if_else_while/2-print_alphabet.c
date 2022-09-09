f8fb3815e1b364013c320af3028a0469ba7d205b
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-09 14:11:35 -0700

    new commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints alphabets in lower case
+ * main - Prints alphabets in lower case
  *
- * return: always 0
+ * Return: Always 0.
+ *
  */
 int main(void)
 {
