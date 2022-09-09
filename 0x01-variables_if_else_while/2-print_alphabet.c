4ceb1261a670caf5ce2397aa34d1b669fff28689
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-09 13:59:43 -0700

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar('\n');
+
+	putchar('\n');
+
+	return (0);
+}
