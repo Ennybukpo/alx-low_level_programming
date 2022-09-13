5333beec5a6da95e4d9cff54dd44d8aefd01830a
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-13 14:49:09 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 2-print_alphabet_x10.c prints 10x the alphabet
+ *
+ * in lower case
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0
+	char alphabets;
+	while (count++ <= 9)
+{
+	for (alphabets = 'a'; alphabets <= 'z'; alphabts++)
+		_putchar(alphabets);
+	_putchar('\n');
+}
+}
