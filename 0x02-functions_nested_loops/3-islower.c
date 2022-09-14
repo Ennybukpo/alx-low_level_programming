fdaa4b952759431aabdfd93c83980f817855dc12
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-14 05:59:29 -0700

    new commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower.c - Checks if a characte is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 if otherwise.
+ *
+ * File: 3-islower.c
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
