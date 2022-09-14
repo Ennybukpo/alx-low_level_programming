e16d436b7b8912a858aef50812efe372bb46eaad
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-14 15:08:21 -0700

    git commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ void print_times_table(int n)
 	{
 		for (b = 0; b <= n; b++)
 		{
-			rep = a*b;
+			rep = a * b;
 			if (b == 0)
 				_putchar('0' + rep);
 			else if (rep < 10)
@@ -27,7 +27,13 @@ void print_times_table(int n)
 			}
 			else if (rep < 100)
 			{
-				_putchar('0' + rep / 100);
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep /100);
 				_putchar('0' + (rep - 100) / 10);
 				_putchar('0' + rep % 10);
 			}
