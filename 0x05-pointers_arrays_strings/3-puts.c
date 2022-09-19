23b2c4c5bab730ebf77732150ee2c2b383d8c8b2
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-19 12:41:23 -0700

    to print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: param
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
