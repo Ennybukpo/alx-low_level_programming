a5c04afb4f489d8a8ec939907121136559d562c9
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-22 04:21:57 -0700

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compares strings
+ * @s1: first string
+ * @s2: second string
+ * Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+	char *str_one = s1;
+	char *str_two = s2;
+
+
+	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+	{
+		str_one++;
+		str_two++;
+	}
+
+	return (*str_one - *str_two);
+}
