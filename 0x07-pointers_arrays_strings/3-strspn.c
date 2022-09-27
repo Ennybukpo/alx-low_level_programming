bfc03b9a38d712e5e47e879a029c1965c223c002
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-26 17:31:57 -0700

    length of string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of prefix substring
+ * @s: segment of string to be scanned
+ * @accept: string that contains list of character to be matched
+ * Return: count
+ **/
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i =0;
+	while (str1[i] != '\0') /*Declaring WHILE *s */
+	{
+		j = 0;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		{
+			if (str2[j] == str1[i]) /*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+			}
+			
+			j++;    /*j increment*/
+		}
+
+		if (s[i] != accept[j]) /*If its not equal*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
+	}
+
+	return (count); /*return the value of count*/
+}
