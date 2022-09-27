b843b2febdbf1862f41b33487e4d9c60884f6b51
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-09-26 18:10:13 -0700

    substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack != '\0')
 	{
 		str1 = haystack; /*values*/
-		str2 = needles;
+		str2 = needle;
 
 		/*star WHILE*/
 		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
