c11f0a8c95bc146014b82bda1550a397526b5530
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-10-05 14:03:44 -0700

    new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print name of program
+ * @argc: Count args
+ * @argv: Arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
