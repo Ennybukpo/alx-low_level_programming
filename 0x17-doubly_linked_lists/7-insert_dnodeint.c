f4fe9cd3fd85a9dff6645e9442acdb967b49034b
Eno-obong Moses Ukpo <ebtandy90@gmail.com>
2022-12-12 06:09:03 -0800

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	new = NULL;
-	if (idk == 0)
+	if (idx == 0) 
 		new = add_dnodeint(h, n);
 	else
 	{
